2ad5a2f081281c90a3dd3deefe5e7ef9e8ff031f
aziaami <aamiraziz7@gmail.com>
2014-04-22 01:12:51 +0100

    Modified BlogHandler to display front.html where users can submit a newpost

--- src/blog/blogHandler.py
+++ src/blog/blogHandler.py
@@ -1,5 +1,21 @@
 from src.BaseHandler import *
 
 class BlogHandler(BaseHandlerClass):
+    def blogTemplate(self):
+    	return '/blog/front.html'
+
+    def render_front(self, title="", blog="", error=""):
+    	self.render(self.blogTemplate(), title=title, blog=blog, error=error)
+
     def get(self):
-        self.write("this works")
+        self.render_front()
+
+    def post(self):
+    	title = self.request.get("title")
+    	blog = self.request.get("blog")
+
+    	if title and blog:
+    		self.write('thanks!')
+    	else:
+    		error = "need both a title and a blog entry, please."
+    		self.render_front(title=title, blog=blog, error=error)
